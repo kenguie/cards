868e07200775f2f43ef7019b362ab25f976e6a56
nyhunter77 <nyhunter77@yahoo.com>
2014-10-23 11:28:18 -0400

    added projects / cleaned up header

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,23 @@
+class ProjectsController < ApplicationController
+
+	before_action :authenticate_user!, only: [:index]
+
+  def index
+  	@projects = current_user.projects
+  end
+
+  def show
+  	@project = Project.find(params[:id])
+  end
+
+  def create
+  	@project = Project.create(title: "New Project", user_id: current_user.id)
+  	redirect_to @project, notice: "Project Created"
+  	# if @project.save
+
+  	# else
+
+  	# end
+  end
+
+end
